1314fcc7ead67e4f913d6e77666272b8a4e33f56
Scott <scott.walker@student.adelaide.edu.au>
2026-04-27 13:32:20 +0930

    Egg + Dinosaur Basic Integration - Set Egg - Prototype eggType for later design - Commented out abstract methods until later integration

--- dinosaur.py
+++ dinosaur.py
@@ -1,11 +1,13 @@
 from abc import ABC, abstractmethod
+from egg import Egg
 # Abstract class
 class Dinosaur(ABC):
     def __init__(self, name:str, gender:str, age:int):
         self.__name = name
         self.__gender = gender
         self.__age = age
-        self.__egg = None
+        self.__egg = []
+        self.__eggType = None
 
     def get_name(self):
         return self.__name
@@ -13,8 +15,10 @@ class Dinosaur(ABC):
         return self.__gender
     def get_age(self):
         return self.__age
-    def get_egg(self):
-        return self.__egg
+    def get_eggs(self):
+        return list(self.__egg)
+    def get_eggType(self):
+        return self.__eggType
     def set_name(self, name):
         if not isinstance(name, str):
             raise TypeError("Name must be a string!")
@@ -24,28 +28,51 @@ class Dinosaur(ABC):
         if not isinstance(gender, str):
             raise TypeError("Gender must be string value!")
         if gender not in ("male", "female"):
-            raise TypeError("Gender must be 'male' or 'female' value")
+            raise ValueError("Gender must be 'male' or 'female' value")
         self.__gender = gender
     def set_age(self, age):
         if not isinstance(age, int):
-            raise TypeError("Age must be int value!")
+            raise TypeError("Age must be int/number value!")
         self.__age = age
+    def set_egg(self, egg):
+        if not self.gender == "female":
+            raise TypeError("Only female dinosaurs can tend to eggs!")
+        if not isinstance(egg, Egg):
+            raise TypeError("Egg must be Egg object!")
+        if not egg.name == self.__eggType:
+            raise TypeError(f"Egg must be {self.eggType} type!")
+        self.__egg.append(Egg(egg))
+
     
     age = property(get_age, set_age)
     name = property(get_name, set_name)
     gender = property(get_gender, set_gender)
+    eggType = property(get_eggType)
+    eggs = property(get_eggs)
 
-
-    @abstractmethod
-    def lay_egg(self):
+    def __str__(self):
+        eggs = "None" if not self.eggs else ", ".join(str(e) for e in self.eggs)
+        return (
+            f"Name: {self.name}\n"
+            f"Type: {self.__class__.__name__}\n"
+            f"Gender: {self.gender}\n" 
+            f"Age: {self.age}\n"
+            f"Eggs: {eggs}\n"
+        )
+    # TODO: Implement this in descendant classes before removing comments
+    #@abstractmethod
+    #def lay_egg(self):
         pass
-    @abstractmethod
-    def warm_egg(self):
+    #@abstractmethod
+    #def warm_egg(self):
         pass
-    @abstractmethod
-    def name_baby(self):
+    #@abstractmethod
+    #def name_baby(self):
         pass
 
 class ConcreteDinosaur(Dinosaur):
     def __init__(self, name, gender, age):
         super().__init__(name, gender, age)
+
+dinosaur = ConcreteDinosaur("Bill", "male", 20)
+print(dinosaur)
